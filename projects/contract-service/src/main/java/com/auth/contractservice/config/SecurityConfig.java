e711cd96c718958f1161da108ceaea0a2ba25e3a
Joyle Silva <joyle.silva@gmail.com>
2025-07-22 22:37:15 -0300

    Ajustada config de server de contract-service.

--- projects/contract-service/src/main/java/com/auth/contractservice/config/SecurityConfig.java
+++ projects/contract-service/src/main/java/com/auth/contractservice/config/SecurityConfig.java
@@ -6,9 +6,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableMethodSecurity
